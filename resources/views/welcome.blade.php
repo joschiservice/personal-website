23466380c13685e044564d16556211e94d03730c
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-06-10 16:38:21 +0200

    design changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <div class="parallax background-1">
-
+    <div class="parallax background-1 d-flex justify-content-center align-items-center">
+        <div class="container">
+            <h1 class="display-4 font-weight-bold">Joschua Haß</h1>
+            <h3>Selbtlernender Software Entwickler durch Forschung, Experimente und Entwicklung.</h3>
+        </div>
     </div>
 @endsection
