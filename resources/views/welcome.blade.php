8635ec2b8a7023815c99840a3085467a7435e16e
Joschua H <joschua.hass@t-online.de>
2020-06-18 20:32:53 +0200

    added text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <div class="parallax background-1 d-flex justify-content-center align-items-center">
         <div class="container animate__animated animate__fadeInDown">
             <h1 class="display-4 font-weight-bold">Joschua Haß</h1>
-            <h3>Selbtlernender Softwareentwickler durch Forschung, Experimente und Entwicklung.</h3>
+            <h3>Selbtlernender Softwareentwickler durch Forschung und Entwicklung auf YouTube</h3>
         </div>
     </div>
     <div style="height:100vh;">
