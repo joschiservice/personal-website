2c892ca433ecea1c9fe4f2fc8898da4b974f4bb5
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-07-12 21:35:26 +0200

    added VTCM project

--- app/Http/Controllers/MainPageController.php
+++ app/Http/Controllers/MainPageController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GrahamCampbell\GitHub\Facades\GitHub;
 
 class MainPageController extends Controller
 {
     public function index(){
-        return view("main");
+        $VTCManagerOrg = GitHub::organization()->show("VTCManager");
+        return view("main", ["VTCManagerOrg" => $VTCManagerOrg]);
     }
 }
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -23,10 +23,10 @@
                         <div class="row">
                             <div class="col-lg">
                                 <div class="card card-custom shadow-lg rounded" style="width: 100%;">
-                                    <img class="card-img-top" src="https://images.hdqwalls.com/wallpapers/2019-tesla-model-y-4k-or.jpg" alt="Card image cap">
+                                    <img class="card-img-top" src="{{$VTCManagerOrg["avatar_url"]}}" alt="VTCManager avatar">
                                     <div class="card-body">
-                                      <h5 class="card-title">Card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                      <h5 class="card-title">VTCManager</h5>
+                                      <p class="card-text">{{$VTCManagerOrg["description"]}}</p>
                                       <a href="#" class="btn btn-primary">Go somewhere</a>
                                     </div>
                                 </div>
