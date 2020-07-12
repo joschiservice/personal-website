615bf7026da51a780f153ec480bb1b7d5a4db2d0
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-07-12 21:25:47 +0200

    added controller for MainPage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        dd(GitHub::me()->organizations());
+        dd(GitHub::me()->repositories());
         return view('home');
     }
 }
