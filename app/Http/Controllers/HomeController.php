3d6c3614c7887d8560a44aad84f314ba0db9e80d
Joschua H <joschua.hass@t-online.de>
2020-07-12 20:07:26 +0200

    added Github API package

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GrahamCampbell\GitHub\Facades\GitHub;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
+        dd(GitHub::me()->organizations());
         return view('home');
     }
 }
