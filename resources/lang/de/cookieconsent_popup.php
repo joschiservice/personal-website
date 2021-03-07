9e1f540bbc791f21f33b80561e09133cedf8c1d7
Joschua H <joschua.hass.sh@gmail.com>
2021-03-07 10:04:27 +0100

    Initialize project using Create React App

--- resources/lang/de/cookieconsent_popup.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'message' => 'Um unsere Webseite für Sie optimal zu gestalten und fortlaufend verbessern zu können, verwenden wir Cookies.',
-    'allow' => 'Alles klar!',
-    'deny' => 'Ablehnen',
-    'learn_more' => 'Weitere Informationen',
-];
--- resources/lang/de/webrepoinfo.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    //SENDER
-    'author' => 'Joschua Haß',
-    'content_p1' => 'Pssst! Wusstest du schon, dass der SourceCode dieser Webseite komplett einsehbar ist? Nein? Dann klick',
-    'content_p2' => 'mich.',
-];
