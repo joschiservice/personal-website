2f93984e360fdefbdc0a67daff94de59d059cb3f
Joschua H <joschua.hass.sh@gmail.com>
2024-01-12 11:28:14 +0100

    feat: download CV

--- app/components/buttons/DownloadCvButton.tsx
+++ app/components/buttons/DownloadCvButton.tsx
@@ -3,7 +3,7 @@ import DownloadIcon from '@mui/icons-material/Download';
 
 export function DownloadCvButton() {
   return (
-    <Button component="label" variant="outlined" startIcon={<DownloadIcon />} sx={{ textTransform: 'none' }}>
+    <Button variant="outlined" startIcon={<DownloadIcon />} sx={{ textTransform: 'none' }} href="/docs/Joschua_Hass_CV.pdf" target="_blank">
       Download CV
     </Button>
   )
