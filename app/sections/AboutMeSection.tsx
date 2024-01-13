486f0ad13189b4267b46e79c9f09daa471701471
Joschua H <joschua.hass.sh@gmail.com>
2024-01-13 14:59:30 +0100

    fix: typos

--- app/sections/AboutMeSection.tsx
+++ app/sections/AboutMeSection.tsx
@@ -10,12 +10,12 @@ export function AboutMeSection() {
           About Me
         </Typography>
         <Typography color={grey[400]}>
-          Back in the days when I was 15 years old and still went to school, I was already very passionate about software engineering. As one of my favorite tools for creating virtual trucking companies with others for the game Euro Truck Simulator 2 decided to shut down its operations,
+          Back in the days when I was 15 years old and still went to school, I was already very passionate about software engineering. As one of my favorite tools for collaborating with others in virtual trucking companies for the game &apos;Euro Truck Simulator 2&apos; decided to shut down its operations,
           I decided to build a replacement application for everyone who still wants to continue using such a tool.
           <br/><br/>
           Currently, I am primarily working on full-stack web, iOS and C# WPF applications. In my free time, I sometimes work on small casual projects, but primarily on <TextLink href="https://www.nextgendrive.net/products/sparky" text="Sparky" />, a companion mobile app for Kia & Hyundai drivers.
           <br/><br/>
-          When I&apos;m not at the computer, I&apos;m usually checking out electric vehicle events, working on my own electric vehicle (a <TextLink text="Kia e-Soul SK3" href="https://www.nextgendrive.net/products/ng-one"/>, hanging out with friends, or playing around in virtual reality.
+          When I&apos;m not at the computer, I&apos;m usually traveling around, working on my own electric vehicle (a <TextLink text="Kia e-Soul SK3" href="https://www.nextgendrive.net/products/ng-one"/>), hanging out with friends, doing photography or playing around in virtual reality.
         </Typography>
       </Container>
     </Box>
