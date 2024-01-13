7bb039af40e9165c869a8faade42734dae57dded
Joschua H <joschua.hass.sh@gmail.com>
2024-01-13 12:35:58 +0100

    feat: smooth scroll to section on same page

--- app/components/navbar/NavbarItem.tsx
+++ app/components/navbar/NavbarItem.tsx
@@ -3,6 +3,9 @@
 import {styled, Typography} from "@mui/material";
 import Link from "next/link";
 import {lightBlue} from "@mui/material/colors";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { Link as ScrollLink } from 'react-scroll';
 
 interface Props {
   title: string;
@@ -18,10 +21,47 @@ const StyledLink = styled(Link)({
   },
 });
 
+const StyledScrollLink = styled(ScrollLink)({
+  color: "white",
+  textDecoration: "none",
+  transition: "color .3s ease-in-out",
+
+  // ScrollLink is not a href, so we need to apply these styles
+  cursor: 'pointer',
+  userSelect: 'none',
+
+  '&:hover': {
+    color: lightBlue[400],
+  },
+});
+
 export function NavbarItem({title, href}: Props) {
+  const currentPathName = usePathname();
+
+  const [currentLinkType, setCurrentLinkType] = useState(0); // 0 = default href link, 1 = smooth scroll link
+  const [targetElement, setTargetElement] = useState('');
+
+  useEffect(() => {
+    const [pathName, elementName] = href.split('#');
+
+    // Is current page?
+    if (pathName == currentPathName && elementName != undefined) {
+      setTargetElement(elementName);
+      setCurrentLinkType(1);
+    } else {
+      setCurrentLinkType(0);
+    }
+  }, [currentPathName, href]);
+
   return (
-    <StyledLink href={href}>
+    currentLinkType == 1 ? (
+      <StyledScrollLink to={targetElement} smooth={true}>
+        <Typography>{title}</Typography>
+      </StyledScrollLink>
+    ) : (
+      <StyledLink href={href}>
       <Typography>{title}</Typography>
     </StyledLink>
+    )
   )
 }
