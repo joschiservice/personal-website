bf120e6a516e9687e6f0b7cc712a17cdbcae4f54
Joschua H <joschua.hass.sh@gmail.com>
2024-01-14 11:22:35 +0100

    fix: removed Navbar from Home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@ import {AboutMeSection} from "@/app/sections/AboutMeSection";
 import {WorkExperienceSection} from "@/app/sections/WorkExperienceSection";
 import {CertificatesSection} from "@/app/sections/CertificatesSection";
 import {ProjectsSection} from "@/app/sections/ProjectsSection";
-import {Navbar} from "@/app/components/navbar";
 import {ToolsSection} from "@/app/sections/ToolsSection";
 import type { Metadata } from 'next';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <HomePageHeroSection />
       <AboutMeSection />
       <WorkExperienceSection />
