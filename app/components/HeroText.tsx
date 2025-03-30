daa27f240c745109c1b88273cc9806cf4d7f5419
Joschua H. <54814804+joschiservice@users.noreply.github.com>
2025-03-30 10:25:58 +0200

    tweak(HeroText): adjust leading

--- app/components/HeroText.tsx
+++ app/components/HeroText.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export function HeroText({ children }: Props) {
   return (
-    <h1 className="text-3xl md:text-4xl lg:text-[4rem] font-bold leading-tight md:leading-[5rem] mb-4
+    <h1 className="text-3xl md:text-4xl lg:text-[4rem] font-bold leading-tight lg:leading-[5rem] mb-4
       [text-shadow:0_0_15px_rgba(255,255,255,0.3)]">
       {children}
     </h1>
