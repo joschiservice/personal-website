1eb84086e3304047a8e8ec5d545fed1666793e92
Joschua H. <54814804+joschiservice@users.noreply.github.com>
2025-07-29 17:30:54 +0200

    feat: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 import { Navbar } from "./components/navbar";
 
@@ -32,6 +33,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Analytics />
       </body>
     </html>
   );
